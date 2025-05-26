da2b756c43b412d2e03f5dbfa0c8784d62f9e7af
fady <fadi.sami.goda@gmail.com>
2025-05-27 01:43:12 +0300

    Added Projects

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -7,6 +7,7 @@ function PortfolioBlock(props) {
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box component={'img'} src={image} alt={'mockup'}/>
+         {/* marginBottom={'7rem'} */}
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,8 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import Architecture from "../img/Architecture.gif"
+import weatherapp from "../img/weatherapp.png"
+import argo from "../img/argo.png"
+import lib from "../img/lib.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -85,8 +84,8 @@ export const info = {
     bio: "Hello! I'm Fady. I'm a Cloud & DevOps engineer.",
     skills:
         {
-            proficientWith: ['DevOps', 'K8S', 'git', 'github', 'Github Actions', 'Ansible', 'AWS', 'python'],
-            exposedTo: ['nodejs', 'Go']
+            proficientWith: ['DevOps', 'Docker', 'K8S', 'git', 'github', 'Github Actions', 'Ansible', 'AWS', 'python', 'MySQL'],
+            exposedTo: ['nodejs', 'Go', 'Flutter', 'React Native']
         }
     ,
     hobbies: [
@@ -111,34 +110,28 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
+            title: "AWS Automation with Terraform",
             // live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/Fady120/AWS-Automation-with-Terraform", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
+            image: Architecture
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
+            title: "Weather App K8S",
+            // live: "https://paytonpierce.dev",
+            source: "https://github.com/Fady120/weatherapp-k8s",
+            image: weatherapp
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
+            title: "My React Portfolio CI/CD",
+            // live: "https://paytonpierce.dev",
+            source: "https://github.com/Fady120/instabug_internship_exam_2023",
+            image: argo
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
+            title: "Library Management System",
+            // live: "https://paytonpierce.dev",
+            source: "https://github.com/Fady120/Library-Management-System",
+            image: lib
         }
     ]
 }
