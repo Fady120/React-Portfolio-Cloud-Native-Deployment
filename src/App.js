618b866b1d6c66cc3fad5e55c75175ec162cc1c7
fady <fadi.sami.goda@gmail.com>
2025-05-26 23:25:48 +0300

    Changed Template Name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 function App() {
    return (
       <div>
-         <BrowserRouter basename="/fady">
+         <BrowserRouter>
             <BaseLayout/>
          </BrowserRouter>
       </div>
